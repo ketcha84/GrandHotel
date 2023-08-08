e44cc9a61bb5ab25c35b1b59d32213f45f200e78
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-08-08 18:57:50 +0200

    Created Project Grand Hotel.

---
+++ src/main/java/pl/grandhotel/grandhotel/GrandHotelApplication.java
@@ -0,0 +1,13 @@
+package pl.grandhotel.grandhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/grandhotel/grandhotel/GrandHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grandhotel.grandhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
