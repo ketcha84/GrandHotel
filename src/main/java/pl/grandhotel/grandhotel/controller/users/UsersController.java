7a7febb7bd4a5b4e22bb58ec6fe684927787d0a8
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-08-08 22:30:50 +0200

    created UsersController

---
+++ src/main/java/pl/grandhotel/grandhotel/controller/users/UsersController.java
@@ -0,0 +1,10 @@
+package pl.grandhotel.grandhotel.controller.users;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/grandhotel")
+public class UsersController {
+
+}
