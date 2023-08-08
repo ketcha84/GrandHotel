0ac4d7d5f5edd7999244a87755a574008bdf67af
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-08-08 22:32:48 +0200

    refactor

--- src/main/java/pl/grandhotel/grandhotel/servises/admin/AdminService.java
+++ src/main/java/pl/grandhotel/grandhotel/servises/admin/AdminService.java
@@ -1,6 +1,5 @@
 package pl.grandhotel.grandhotel.servises.admin;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.grandhotel.grandhotel.model.Account;
 import pl.grandhotel.grandhotel.repositories.Adminrepository;
