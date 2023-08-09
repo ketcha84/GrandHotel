6962899dd2a25c3c41c8ec380d82b222a571d15b
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-08-09 20:23:45 +0200

    hello

--- src/main/java/pl/grandhotel/grandhotel/controller/users/UsersController.java
+++ src/main/java/pl/grandhotel/grandhotel/controller/users/UsersController.java
@@ -1,10 +1,22 @@
 package pl.grandhotel.grandhotel.controller.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.grandhotel.grandhotel.servises.users.UserService;
 
 @Controller
 @RequestMapping("/grandhotel")
 public class UsersController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public String hello(Model model) {
+        return "/grandHotel/index.html";
+    }
 
 }
