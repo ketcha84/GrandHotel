d7f3abd1a87aa1693b8c8705454676fb3dcacebc
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-08-28 21:12:26 +0200

    OrdersController is begin

--- src/main/java/pl/grandhotel/grandhotel/controller/users/OrdersController.java
+++ src/main/java/pl/grandhotel/grandhotel/controller/users/OrdersController.java
@@ -2,14 +2,43 @@ package pl.grandhotel.grandhotel.controller.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.grandhotel.grandhotel.servises.BookingService;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.grandhotel.grandhotel.exceptions.OrderExceptions.OrderException;
+import pl.grandhotel.grandhotel.model.Order;
+import pl.grandhotel.grandhotel.servises.OrderService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/order")
 public class OrdersController {
     @Autowired
-    BookingService bookingService;
+    OrderService orderService;
+
+    @GetMapping()
+    @ResponseBody
+    public List<Order> getOrders(Model model) {
+        var l = orderService.getOrders();
+        model.addAttribute("orders", l);
+        return l;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Order getById(@PathVariable("id") Integer id) {
+        try {
+            var o = orderService.getOrderById(id);
+            System.out.println(o);
+            return o;
+        } catch (OrderException e) {
+            System.err.println(e);
+        }
+        return null;
+    }
 
 
 }
